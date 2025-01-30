173fa5c05b72e6b0c7170f34ddb9ce518f0dee55
jnabulsi <jordanabulsi@gmail.com>
2025-01-30 18:19:54 +1100

    header and footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,66 @@
+<template>
+  <footer class="footer">
+    <div class="footer-content">
+      <!-- LinkedIn Icon -->
+      <a href="https://www.linkedin.com/in/tess-salinger-b865972b9/" target="_blank" rel="noopener noreferrer"
+        class="icon-link">
+        <img src="/img/linkedin.png" alt="LinkedIn" class="icon" />
+      </a>
+
+      <!-- YouTube Icon -->
+      <a href="https://www.youtube.com/channel/UCmobJQ0i_DJEEEKBqYTpfiQ" target="_blank" rel="noopener noreferrer"
+        class="icon-link">
+        <img src="/img/youtube.png" alt="YouTube" class="icon" />
+      </a>
+
+      <!-- Instagram Icon -->
+      <a href="https://www.instagram.com/thisismyperception/?hl=en" target="_blank" rel="noopener noreferrer"
+        class="icon-link">
+        <img src="/img/instagram.png" alt="Instagram" class="icon" />
+      </a>
+
+      <!-- Vimeo Icon -->
+      <a href="https://vimeo.com/user54674996" target="_blank" rel="noopener noreferrer" class="icon-link">
+        <img src="/img/vimeo.png" alt="Vimeo" class="icon" />
+      </a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter',
+};
+</script>
+
+<style scoped>
+.footer {
+  background-color: orange;
+  padding: 20px 0;
+  text-align: center;
+}
+
+.footer-content {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+}
+
+.icon-link {
+  display: inline-block;
+  transition: transform 0.3s ease, opacity 0.3s ease;
+}
+
+.icon-link:hover {
+  transform: scale(1.1);
+  /* Slightly enlarge icons on hover */
+  opacity: 0.8;
+  /* Slightly fade icons on hover */
+}
+
+.icon {
+  width: 32px;
+  /* Adjust icon size as needed */
+  height: 32px;
+}
+</style>
---
+++ components/AppHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <header class="header">
+    <NuxtLink to="/" class="logo">
+      <img src="/img/logo.png" alt="Website Logo" class="logo-image" />
+    </NuxtLink>
+
+    <BurgerMenu />
+  </header>
+</template>
+
+<script>
+import BurgerMenu from '~/components/BurgerMenu.vue';
+
+export default {
+  name: 'Head',
+  components: {
+    BurgerMenu,
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 200px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.logo {
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+}
+
+.logo-image {
+  height: 120px;
+  width: auto;
+  padding: 40px 40px;
+}
+</style>
